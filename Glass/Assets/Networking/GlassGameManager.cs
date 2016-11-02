12f02ca4ef657c804977f54fe3c972a3828a8473
Gloria Budiman <wahyu.g@gmail.com>
2016-11-02 02:57:50 -0700

    Fixed zombie objects no longer appear on dis/reconnections.

--- Glass/Assets/Networking/GlassGameManager.cs
+++ Glass/Assets/Networking/GlassGameManager.cs
@@ -1,21 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GlassGameManager : Photon.PunBehaviour {
   public GameObject capsule_test_prefab;
 
+  Text debugt;
+  List<int> actor_ids = new List<int>();
+
   // Use this for initialization
   void Start () {
-    
+    CleanUpObjects();
     if (PlayerManager.local_player_instance == null) {
       print("attempting to create capsule object ...");
       PhotonNetwork.Instantiate(capsule_test_prefab.name, new Vector3(0, 0, 0), Quaternion.identity, 0);
     } else {
       print("ignoring scene load");
     }
-    
-    if (PhotonNetwork.isMasterClient) { InvertCamera(); }
+
+    if (PhotonNetwork.isMasterClient) {
+      //PhotonNetwork.Instantiate(debug_text.name, new Vector3(0, -3, 0), Quaternion.identity, 0);
+      InvertCamera();
+    }
 	}
 	
 	// Update is called once per frame
@@ -47,11 +55,17 @@ public class GlassGameManager : Photon.PunBehaviour {
 
   public override void OnPhotonPlayerConnected(PhotonPlayer other_player) {
     Debug.Log("Player connected: " + other_player + " with id " + other_player.ID);
+    PhotonNetwork.DestroyPlayerObjects(PhotonNetwork.player.ID);
+    CleanUpActorIDsExceptThis(PhotonNetwork.player.ID);
+    actor_ids.Add(other_player.ID);
+    CleanUpObjects();
 
     if (PhotonNetwork.isMasterClient) {
       Debug.Log("Upon connection: I am master client");
       LoadArena();
     }
+
+    
     //base.OnPhotonPlayerConnected(newPlayer);
   }
 
@@ -62,5 +76,46 @@ public class GlassGameManager : Photon.PunBehaviour {
       Debug.Log("Upon disconnection: I am master client");
       LoadArena();
     }
+  }
+
+  void CleanUpActorIDsExceptThis(int id) {
+    actor_ids = new List<int>();
+    actor_ids.Add(id);
+  }
+
+  string DebugPrintList(List<int> ls) {
+    string s = "In-game players: ";
+    foreach (int l in ls) {
+      s += l + ", ";
+    }
+
+    //print(s);
+    return s;
+  }
+
+  void CleanUpObjects() {
+    print("cleaning up...");
+    actor_ids = new List<int>();
+    foreach (PhotonPlayer p in PhotonNetwork.playerList) {
+      actor_ids.Add(p.ID);
+    }
+
+    print(DebugPrintList(actor_ids));
+    PhotonView[] photons = GameObject.FindObjectsOfType<PhotonView>();
+    foreach (PhotonView photon in photons) {
+      if (!IsInActorIDs(photon.owner.ID)) {
+        Destroy(photon.gameObject);
+      }
+    }
+  }
+
+  bool IsInActorIDs(int id) {
+    foreach (int i in actor_ids) {
+      if (i == id) {
+        return true;
+      }
+    }
+
+    return false;
   }
 }
