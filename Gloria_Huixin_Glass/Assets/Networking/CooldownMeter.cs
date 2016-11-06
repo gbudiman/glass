415104334e38af7cfbcfcc18b0d6a7b22d798b90
Gloria Budiman <wahyu.g@gmail.com>
2016-11-06 14:59:26 -0800

    Powerup Cooldown implemented.

---
+++ Gloria_Huixin_Glass/Assets/Networking/CooldownMeter.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CooldownMeter : MonoBehaviour {
+  LineRenderer lr;
+  Color blend_color;
+
+  float base_cooldown;
+  float cooldown_time;
+  bool cooldown_active;
+
+  public float BaseCooldown {
+    set {
+      base_cooldown = value;
+    }
+  }
+
+  public bool IsInCooldown {
+    get { return cooldown_active; }
+  }
+
+	// Use this for initialization
+	void Start () {
+    blend_color = new Color(0x5a, 0x5a, 0x5a, 0.15f);
+    lr = GetComponent<LineRenderer>();
+    lr.SetColors(blend_color, blend_color);
+    lr.sortingLayerName = "PowerupElementCooldown";
+    cooldown_active = false;
+	}
+
+  public void Activate() {
+    cooldown_active = true;
+    cooldown_time = base_cooldown;
+    lr.SetPosition(1, new Vector3(1, 0, 0));
+  }
+	
+	// Update is called once per frame
+	void Update () {
+    TickCooldownTimer();
+	}
+
+  void TickCooldownTimer() {
+    if (!cooldown_active) { return; }
+    cooldown_time -= Time.deltaTime;
+    RenderCooldownMask();
+
+    if (cooldown_time < 0) {
+      cooldown_active = false;
+    }
+  }
+
+  void RenderCooldownMask() {
+    float pos_x = cooldown_time / base_cooldown * 1.0f;
+    lr.SetPosition(1, new Vector3(pos_x, 0, 0));
+  }
+}
--- Gloria_Huixin_Glass/Assets/Networking/PowerUpElement.cs
+++ Gloria_Huixin_Glass/Assets/Networking/PowerUpElement.cs
@@ -6,7 +6,9 @@ public class PowerUpElement : MonoBehaviour {
   public enum PowerUpType { pu_triple_shot, pu_safety_net };
   public PowerUpType powerup_type;
   PowerUpUI powerup_ui;
+  PowerupMeter powerup_meter;
   KeyboardController kbc;
+  CooldownMeter cdm;
 
   public float PowerRequirement {
     get {
@@ -22,7 +24,19 @@ public class PowerUpElement : MonoBehaviour {
 	void Start () {
     animator = GetComponentInChildren<Animator>();
     powerup_ui = transform.parent.GetComponentInChildren<PowerUpUI>();
+    powerup_meter = GameObject.FindObjectOfType<PowerupMeter>();
+    InitializeCooldownMeter();
 	}
+
+  void InitializeCooldownMeter() {
+    cdm = GetComponentInChildren<CooldownMeter>();
+    if (cdm == null) { return; }
+
+    switch(powerup_type) {
+      case PowerUpType.pu_triple_shot: cdm.BaseCooldown = 10.0f; break;
+      case PowerUpType.pu_safety_net: cdm.BaseCooldown = 20.0f; break;
+    }
+  }
 	
 	// Update is called once per frame
 	void Update () {
@@ -31,13 +45,17 @@ public class PowerUpElement : MonoBehaviour {
   void OnMouseDown() {
     switch(powerup_type) {
       case PowerUpType.pu_triple_shot:
-        FindSubject();
-        CheckPrerequisite();
-        kbc.SetTripleShot();
+        if (CheckPrerequisite()) {
+          FindSubject();
+          kbc.SetTripleShot();
+          cdm.Activate();
+        }
+        
         break;
       case PowerUpType.pu_safety_net:
         if (CheckPrerequisite()) {
           FindSafetyNet();
+          cdm.Activate();
         }
         break;
     }
@@ -45,7 +63,7 @@ public class PowerUpElement : MonoBehaviour {
   }
 
   bool CheckPrerequisite() {
-    if (!GetComponentInChildren<DisablerMask>().GetComponent<SpriteRenderer>().enabled) {
+    if (!GetComponentInChildren<DisablerMask>().GetComponent<SpriteRenderer>().enabled && !cdm.IsInCooldown) {
       animator.SetBool("is_clicked", true);
       return true;
     }
@@ -58,6 +76,7 @@ public class PowerUpElement : MonoBehaviour {
       bool is_mine = sfn.GetComponent<PhotonView>().isMine;
       if (!PhotonNetwork.connected || is_mine) {
         sfn.SetEnable(true);
+        powerup_meter.ExecuteSubtract(1);
         break;
       }
     }
