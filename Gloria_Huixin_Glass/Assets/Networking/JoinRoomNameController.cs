560ad86553fc0c7f21752c5808efff0e503937b9
Gloria Budiman <wahyu.g@gmail.com>
2016-11-06 17:18:06 -0800

    Interactive Join Room.

---
+++ Gloria_Huixin_Glass/Assets/Networking/JoinRoomNameController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class JoinRoomNameController : MonoBehaviour {
+  public GameObject join_button;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  public void UpdateInteractivity(string s) {
+    join_button.GetComponent<Button>().interactable = s.Length > 0;
+  }
+}
