1c3f823366501cfbcca36ecdd14479411287c6f0
Huixin Luo <huixinlu@usc.edu>
2016-11-07 11:55:22 -0800

    add paddle

---
+++ Gloria_Huixin_Glass/Assets/PaddleController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour {
+	int hitPoint = 0;
+	public int destroyPoint = 2;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+
+	
+	}
+
+
+	void OnCollisionEnter2D(Collision2D coll) 
+	{
+		hitPoint++;
+		Debug.Log("Collide!" +hitPoint);
+		if(hitPoint == destroyPoint)
+		{
+			Destroy(this);
+		}
+	}
+}
---
+++ Gloria_Huixin_Glass/Assets/TouchDetection.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchDetection: MonoBehaviour {
+
+	Vector3 firstTouchPosition;
+	Vector3 firstReleasePosition;
+	float distance;
+	Vector2 dir;
+	float angle;
+	[SerializeField] GameObject squarePrefab;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))//click
+		{
+			firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			firstTouchPosition.z += 5f;
+
+		}
+
+		if(Input.GetMouseButton(0))//on hold
+		{
+			firstReleasePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			firstReleasePosition.z += 5f;
+			//DrawLine();
+			//Debug.Log("distance: "+ distance);
+			Destroy(DrawLine(), 0.03f);
+
+
+		}
+
+		if(Input.GetMouseButtonUp(0))//release
+		{
+			firstReleasePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			firstReleasePosition.z += 5f;
+
+			DrawLine();
+			Debug.Log("distance: "+ distance);
+
+		}
+
+
+
+
+	
+	}
+
+
+	GameObject DrawLine()
+	{
+		
+		GameObject square1 = (GameObject) Instantiate(squarePrefab, firstTouchPosition, Quaternion.identity);
+
+		//rotate
+		dir = firstReleasePosition - firstTouchPosition;
+		angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		square1.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); // z axis
+
+		//scale
+		distance = Vector3.Distance(firstReleasePosition, firstTouchPosition);
+		square1.transform.localScale += new Vector3(distance, 0, 0);
+
+		return square1;
+
+	}
+
+
+}
+
