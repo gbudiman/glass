b1e11335ceff666af2356ee7c767c55155d95538
Gloria Budiman <wahyu.g@gmail.com>
2016-11-29 20:03:22 -0800

    Fix missing texture on Android (use Trilinear filtering instead of Bilinear). Fix tutorial button positioning.

---
+++ Gloria_Huixin_Glass/Assets/QuickRefController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class QuickRefController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    HandleKeyboardInput();
+	}
+
+  void HandleKeyboardInput() {
+    if (Input.GetKeyDown(KeyCode.Escape)) {
+      GoBackHome();
+    }
+  }
+
+  public void GoBackHome() {
+    SceneManager.LoadScene("Launcher");
+  }
+}
