4e2a8998be2e082e214f831b14e654b26ad8e638
Gloria Budiman <wahyu.g@gmail.com>
2016-12-05 21:11:50 -0800

    ReadySetGo has sound effect.

--- Gloria_Huixin_Glass/Assets/Networking/RSGController.cs
+++ Gloria_Huixin_Glass/Assets/Networking/RSGController.cs
@@ -5,10 +5,14 @@ using System.Collections.Generic;
 public class RSGController : MonoBehaviour {
   const float base_period = 1.0f;
   float timer = 0;
+  AudioSource audio_source;
+  public AudioClip ding_clip;
+  public AudioClip dong_clip;
 
   Stack<string> orders = new Stack<string>(new[] { "Go", "Set", "Ready" });
   // Use this for initialization
   void Start () {
+    audio_source = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -33,6 +37,11 @@ public class RSGController : MonoBehaviour {
 
       if (animator_name == orders.Peek()) {
         animator.enabled = true;
+        if (orders.Count > 1) {
+          audio_source.PlayOneShot(ding_clip);
+        } else {
+          audio_source.PlayOneShot(dong_clip);
+        }
         orders.Pop();
         break;
       }
